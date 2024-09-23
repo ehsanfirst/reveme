7ee4f2b2cf4f7f17689bee5b3aa1a54b21a17424
ehsan <eartl2219@gmail.com>
2024-09-23 22:46:56 +0330

    Starting

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reveme</title>
+    <style>
+        p:hover{
+            color: violet;
+            transform: translate(50px);
+            transition: all ease 2s;
+
+        }
+
+
+    </style>
+</head>
+<body>
+
+    <h1> <? echo "Hello World" ?> </h1>
+
+        <p>Hello from index . html</p>
+    
+
+</body>
+</html>
